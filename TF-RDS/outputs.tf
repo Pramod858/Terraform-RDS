5e2ca0463619e7dd189f528e237f6a5718121930
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2024-07-31 14:52:12 +0530

    Update outputs.tf

--- TF-RDS/outputs.tf
+++ TF-RDS/outputs.tf
@@ -2,3 +2,8 @@ output "rds_writer_endpoint" {
     description = "The writer endpoint of the RDS cluster"
     value       = aws_rds_cluster.aurora_mysql.endpoint
 }
+
+output "instance_public_ip" {
+    description = "The public IP address of the EC2 instance"
+    value       = aws_instance.web.public_ip
+}
